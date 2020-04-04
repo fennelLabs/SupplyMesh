5e3eef30bc369c2985c53dd3497aa0bebbb74d15
Sean Batzel <romulus108@protonmail.com>
2020-04-04 16:16:30 -0400

    Methods.

--- supplymesh/src/components/SupplyMesh.vue
+++ supplymesh/src/components/SupplyMesh.vue
@@ -44,7 +44,103 @@ export default {
           })
         }
       })
-    }
+    },
+    acceptBid (event) {
+      this.winEvent = null
+      this.pending = true
+      this.$store.state.contractInstance().bet(event.target.innerHTML, {
+        gas: 300000,
+        value: this.$store.state.web3.web3Instance().toWei(this.amount, 'ether'),
+        from: this.$store.state.web3.coinbase
+      }, (err, result) => {
+        if (err) {
+          console.log(err)
+          this.pending = false
+        } else {
+          let Won = this.$store.state.contractInstance().Won()
+          Won.watch((err, result) => {
+            if (err) {
+              console.log('could not get event Won()')
+            } else {
+              this.winEvent = result.args
+              this.winEvent._amount = parseInt(result.args._amount, 10)
+              this.pending = false
+            }
+          })
+        }
+      })},
+    addBounty (event) {
+      this.winEvent = null
+      this.pending = true
+      this.$store.state.contractInstance().bet(event.target.innerHTML, {
+        gas: 300000,
+        value: this.$store.state.web3.web3Instance().toWei(this.amount, 'ether'),
+        from: this.$store.state.web3.coinbase
+      }, (err, result) => {
+        if (err) {
+          console.log(err)
+          this.pending = false
+        } else {
+          let Won = this.$store.state.contractInstance().Won()
+          Won.watch((err, result) => {
+            if (err) {
+              console.log('could not get event Won()')
+            } else {
+              this.winEvent = result.args
+              this.winEvent._amount = parseInt(result.args._amount, 10)
+              this.pending = false
+            }
+          })
+        }
+      })},
+    payBounty (event) {
+      this.winEvent = null
+      this.pending = true
+      this.$store.state.contractInstance().bet(event.target.innerHTML, {
+        gas: 300000,
+        value: this.$store.state.web3.web3Instance().toWei(this.amount, 'ether'),
+        from: this.$store.state.web3.coinbase
+      }, (err, result) => {
+        if (err) {
+          console.log(err)
+          this.pending = false
+        } else {
+          let Won = this.$store.state.contractInstance().Won()
+          Won.watch((err, result) => {
+            if (err) {
+              console.log('could not get event Won()')
+            } else {
+              this.winEvent = result.args
+              this.winEvent._amount = parseInt(result.args._amount, 10)
+              this.pending = false
+            }
+          })
+        }
+      })},
+    fundBounty (event) {
+      this.winEvent = null
+      this.pending = true
+      this.$store.state.contractInstance().bet(event.target.innerHTML, {
+        gas: 300000,
+        value: this.$store.state.web3.web3Instance().toWei(this.amount, 'ether'),
+        from: this.$store.state.web3.coinbase
+      }, (err, result) => {
+        if (err) {
+          console.log(err)
+          this.pending = false
+        } else {
+          let Won = this.$store.state.contractInstance().Won()
+          Won.watch((err, result) => {
+            if (err) {
+              console.log('could not get event Won()')
+            } else {
+              this.winEvent = result.args
+              this.winEvent._amount = parseInt(result.args._amount, 10)
+              this.pending = false
+            }
+          })
+        }
+      })}
   },
   mounted () {
     console.log('dispatching getContractInstance')
